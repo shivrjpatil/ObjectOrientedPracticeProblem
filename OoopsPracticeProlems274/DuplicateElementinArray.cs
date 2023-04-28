87b152e030aa7fe6a891000f81c1fca3f5b22d2e
shivrjpatil <shivrajp972@gmail.com>
2023-04-28 21:02:06 +0530

    Added Program to Find duplicate elements in array

---
+++ OoopsPracticeProlems274/DuplicateElementinArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoopsPracticeProlems274
+{
+    internal class DuplicateElementinArray
+    {
+        public void Element()
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 4, 5, 5 };
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Total number of duplicate elements in the array: {0}", count);
+        }
+    }
+}
---
+++ OoopsPracticeProlems274/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoopsPracticeProlems274
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Duplicate Elements in array");
+            DuplicateElementinArray array=new DuplicateElementinArray();
+            array.Element();
+            Console.ReadLine();
+        }
+    }
+}
